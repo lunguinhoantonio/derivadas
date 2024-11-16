cb855b50284da5b94dda3e1c7ae1241c709a94c3
lunguinhoantonio <162999553+lunguinhoantonio@users.noreply.github.com>
2024-11-16 15:49:08 -0300

    ordenado de acordo com a tabela

--- main.c
+++ main.c
@@ -26,7 +26,7 @@ void derivada_polinomio(double a, double n, double b, double c) {
 // Função que calcula a derivada de f(x) = sin(ax + b)
 void derivada_seno(double a, double b) {
     printf("\nA derivada de f(x) = sin(%.0fx + %.0f)\n", a, b);
-    printf("f'(x) = %.0fcos(%.0fx + %.0f)\n\n", a, a, b);
+    printf("f'(x) = cos(%.0fx + %.0f)\n\n", a, b);
 }
 
 // Função que exibe a derivada de f(x) = e^(ax + b)
@@ -40,16 +40,26 @@ int main() {
     double a, b, c, n;
 
     printf("Escolha o tipo de função para calcular a derivada:\n");
-    printf("1. Polinômio (ax^n + bx + c)\n");
-    printf("2. Seno (sin(ax + b))\n");
-    printf("3. Exponencial (e^(ax + b))\n");
-    printf("4. Função simples (f(x) = a)\n");
-    printf("5. Função simples com coeficiente (f(x) = ax)\n");
+    printf("1. Função simples (f(x) = a)\n");
+    printf("2. Função simples com coeficiente (f(x) = ax)\n");
+    printf("3. Polinômio (ax^n + bx + c)\n");
+    printf("4. Seno (sin(ax + b))\n");
+    printf("5. Exponencial (e^(ax + b))\n");
     printf("Sua escolha é: ");
     scanf("%d", &escolha);
 
     switch (escolha) {
         case 1:
+            printf("\nDigite o valor de a: ");
+            scanf(" %lf", &a);
+            derivada_x(a);
+            break;
+        case 2:
+            printf("\nDigite o coeficiente a (para o termo ax): ");
+            scanf(" %lf", &a);
+            derivada_coeficiente_x(a);
+            break;
+        case 3:
             printf("\nDigite o coeficiente a (para o termo ax^n): ");
             scanf("%lf", &a);
             printf("Digite o expoente n (para o termo ax^n): ");
@@ -60,34 +70,23 @@ int main() {
             scanf("%lf", &c);
             derivada_polinomio(a, n, b, c);
             break;
-        case 2:
+        case 4:
             printf("\nDigite o coeficiente a (para o termo ax): ");
             scanf("%lf", &a);
             printf("Digite o coeficiente b (termo constante): ");
             scanf("%lf", &b);
             derivada_seno(a, b);
             break;
-        case 3:
+        case 5:
             printf("\nDigite o coeficiente a (para o termo ax): ");
             scanf("%lf", &a);
             printf("Digite o coeficiente b (termo constante): ");
             scanf("%lf", &b);
             derivada_exp(a, b);
             break;
-        case 4:
-            printf("\nDigite o valor de a: ");
-            scanf(" %lf", &a);
-            derivada_x(a);
-            break;
-        case 5:
-            printf("\nDigite o coeficiente a (para o termo ax): ");
-            scanf(" %lf", &a);
-            derivada_coeficiente_x(a);
-            break;
         default:
             printf("\nEscolha inválida.\n");
             break;
     }
-
     return 0;
 }
